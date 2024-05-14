d35313cb89c0c8de3b689e5df05518a3dc60f171
Muhammd-hussain <163119384+Muhammd-hussain@users.noreply.github.com>
2024-05-14 22:43:43 +0500

    Muhammad hussain.sql

--- Muhammad hussain.sql
+++ Muhammad hussain.sql
@@ -1,22 +1,38 @@
-CREATE TABLE Customers (
-    CustomerID INT AUTO_INCREMENT PRIMARY KEY,
+-- PORJECT:
+-- BANK MANAGEMENT SYSTEM
+-- GROUP MEMBERS: 
+-- 1. Muhammad Hussain (BSE-23F-059)
+-- 2. Muhammad Taimoor (BSE-23F-086)
+-- 3. Sohaib Raheem (BSE-23F-075)
+ 
+CREATE DATABASE BANK_MANAGEMENT_SYSTEM;
+USE BANK_MANAGEMENT_SYSTEM;
+
+CREATE TABLE Employees (
+    EmployeeID INT AUTO_INCREMENT PRIMARY KEY,
     Name VARCHAR(255),
-    Address VARCHAR(255),
-    Phone VARCHAR(20),
-    Email VARCHAR(255)
+    Position VARCHAR(100),
+    Salary DECIMAL(15, 2),
+    BranchID INT,
+    FOREIGN KEY (BranchID) REFERENCES Branches(BranchID)
 );
-CREATE TABLE Accounts (
-    AccountID INT AUTO_INCREMENT PRIMARY KEY,
-    CustomerID INT,
-    AccountType VARCHAR(50),
-    Balance DECIMAL(15, 2),
-    OpenDate DATE,
-    FOREIGN KEY (CustomerID) REFERENCES Customers(CustomerID)
+
+CREATE TABLE Departments (
+    DepartmentID INT AUTO_INCREMENT PRIMARY KEY,
+    DepartmentName VARCHAR(100)
 );
-CREATE TABLE Cards (
-    CardID INT AUTO_INCREMENT PRIMARY KEY,
-    AccountID INT,
-    CardType VARCHAR(50),
-    ExpiryDate DATE,
-    FOREIGN KEY (AccountID) REFERENCES Accounts(AccountID)
+
+CREATE TABLE Branches (
+    BranchID INT AUTO_INCREMENT PRIMARY KEY,
+    Location VARCHAR(255),
+    ManagerID INT,
+    FOREIGN KEY (ManagerID) REFERENCES Employees(EmployeeID)
+);
+
+CREATE TABLE Salaries (
+    SalaryID INT AUTO_INCREMENT PRIMARY KEY,
+    EmployeeID INT,
+    Amount DECIMAL(15, 2),
+    PaymentDate DATE,
+    FOREIGN KEY (EmployeeID) REFERENCES Employees(EmployeeID)
 );
